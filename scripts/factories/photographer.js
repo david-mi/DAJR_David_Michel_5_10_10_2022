2b1692b46675b6e2b9e343f7aebbb8c25257fda8
David Michel <david.michel2809@gmail.com>
2022-10-10 15:18:08 +0200

    feat: completing gerUserCardDom #2

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const linkNode = document.createElement("a")
+        linkNode.href = `/photographer.html?id=${id}`
+        const articleNode = document.createElement("article")
+        const imgNode = document.createElement("img")
+        imgNode.setAttribute("src", picture)
+        const h2Node = document.createElement("h2")
+        h2Node.textContent = name
+
+        const infosNode = document.createElement("p")
+        infosNode.classList.add("infos")
+        const locationNode = document.createElement("strong")
+        locationNode.classList.add("location")
+        locationNode.innerText = `${city}, ${country}`
+        const taglineNode = document.createElement("span")
+        taglineNode.classList.add("tag-line")
+        taglineNode.innerText = tagline
+        const priceNode = document.createElement("span")
+        priceNode.classList.add("price")
+        priceNode.innerText = `${price}€/jour`
+
+        linkNode.append(imgNode, h2Node)
+        infosNode.append(locationNode, taglineNode, priceNode)
+        articleNode.append(linkNode, infosNode)
+        return (articleNode)
     }
     return { name, picture, getUserCardDOM }
 }
