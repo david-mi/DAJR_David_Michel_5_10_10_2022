672ca12b4aa66878bf7f2e38cb472767d392c0b2
David Michel <david.michel2809@gmail.com>
2022-10-10 15:46:28 +0200

    refacto: passing every css classes to kebab-case #5

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,7 +1,6 @@
 import { displayModal, closeModal } from "../utils/contactForm.js"
 
-const contactButton = document.querySelector(".contact_button")
+const contactButton = document.querySelector(".contact-button")
 contactButton.addEventListener("click", displayModal)
 const closeModalImg = document.querySelector(".modal header img")
-console.log(closeModalImg)
 closeModalImg.addEventListener("click", closeModal)
