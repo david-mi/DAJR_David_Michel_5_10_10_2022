b39f9c503475a9f7d96782dc3872f323cbd1fe9e
David Michel <david.michel2809@gmail.com>
2022-10-11 11:09:22 +0200

    doc: adding mediaTypes and PhotographerType in types.js #9

---
+++ data/types.js
@@ -0,0 +1,28 @@
+/**
+* @typedef mediaType
+* @type {{
+  *  id: number,
+  *  photographerId: number,
+  *  title: string,
+  *  image: string,
+  *  likes: number,
+  *  date: string,
+  *  price: number,
+  * }}
+  */
+
+/**
+* @typedef photographerType
+* @type {{
+*  name: string,
+*  id: number,
+*  city: string,
+*  country: string,
+*  tagline: string,
+*  price: number,
+*  portrait: string
+* }}
+*/
+
+
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,5 @@
 import { displayModal, closeModal } from "../utils/contactForm.js"
+import "../../data/types.js"
 
 const contactButton = document.querySelector(".contact-button")
 contactButton.addEventListener("click", displayModal)
