b64d60d2b3b60bc375c1d9d7dde9d32616c96f02
David Michel <david.michel2809@gmail.com>
2022-10-13 13:58:03 +0200

    refacto: returning object from photographerFactory insted of method invocation. Removing currentPathName

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,33 +1,31 @@
 export function photographerFactory(data) {
-    const { name, city, country, tagline, price, id, portrait } = data
+  const { name, city, country, tagline, price, id, portrait } = data
 
-    const currentPathName = location.pathname.replace(/\/|\..+/g, "")
+  const photographerHtmlCard = {
+    index: () => (
+      `<article>
+         <a href="/photographer.html?id=${id}">
+           <img src="assets/photographers/${portrait}">
+           <h2>${name}</h2>
+         </a>
+         <p class="infos">
+           <strong class="location">${city}, ${country}</strong>
+           <span class="tag-line">${tagline}</span>
+           <span class="price">${price}€/jour</span>
+         </p>
+       </article>`
+    ),
+    photographer: () => (
+      ` <h1>${name}</h1>
+        <p>
+          <strong class="location">${city}, ${country}</strong>
+          <span class="tagline">${tagline}</span>
+        </p>
+        <img src="assets/photographers/${portrait}" alt=${name}>`
+    )
+  }
 
-    const photographerHtmlCard = {
-        index: () => (
-            `<article>
-                <a href="/photographer.html?id=${id}">
-                  <img src="assets/photographers/${portrait}">
-                  <h2>${name}</h2>
-                </a>
-                <p class="infos">
-                  <strong class="location">${city}, ${country}</strong>
-                  <span class="tag-line">${tagline}</span>
-                  <span class="price">${price}€/jour</span>
-                </p>
-            </article>`
-        ),
-        photographer: () => (
-            ` <h1>${name}</h1>
-              <p>
-                <strong class="location">${city}, ${country}</strong>
-                <span class="tagline">${tagline}</span>
-              </p>
-              <img src="assets/photographers/${portrait}" alt=${name}>`
-        )
-    }
-
-    return photographerHtmlCard[currentPathName]()
+  return photographerHtmlCard
 }
 
 
