3f926729e661dcf879a77327a33c5d007e5dfdae
David Michel <david.michel2809@gmail.com>
2022-10-14 11:13:54 +0200

    feat: adding validator file, input listeners and handleSubmit logic #13

--- scripts/pages/photographer/form/form.js
+++ scripts/pages/photographer/form/form.js
@@ -1,19 +1,45 @@
 import { form } from "../constants.js"
+import { validator } from "./validators.js"
 import "./types.js"
-const formModalTitleElement = document.querySelector(".modal header h2")
 
-export function handleSubmit(event) {
-  event.preventDefault()
-  console.log("submit")
-}
+const formModalTitleElement = document.querySelector(".modal header h2")
+const inputs = form.querySelectorAll("input[name]")
+
+// Adding a listener to each inputs with the associated validator method as callback
+
+inputs.forEach(input => {
+  if (input.name === "firstName" || input.name === "lastName") {
+    input.addEventListener("input", validator.name)
+  } else {
+    input.addEventListener("input", validator[input.name])
+  }
+})
 
 /**
  * Insert photographers name into form title
  * 
- * @param {*} photographer photographer's name
+ * @param {string} photographer photographer's name
  */
 
 export function displayPhotographerNameToForm(photographer) {
   formModalTitleElement.innerText += ` ${photographer}`
 }
 
+
+/**
+ * Submit form and check if fields are valid
+ * if yes, making an {@link formBodyType} object with input values
+ * 
+ * @param {Event} event 
+ */
+
+export function handleSubmit(event) {
+  event.preventDefault()
+  const isFormValid = form.reportValidity()
+
+  if (isFormValid) {
+    const formData = new FormData(form)
+    const formBody = Object.fromEntries(formData.entries())
+    console.log(formBody)
+  }
+}
---
+++ scripts/pages/photographer/form/validators.js
@@ -0,0 +1,59 @@
+import { form } from "../constants.js"
+
+const nameEnum = {
+  firstName: "prénom",
+  lastName: "nom"
+}
+
+export const validator = {
+
+  /**
+   * Handle firstName and lastName inputs
+   * verifity if input value is longer than 2 caracters and if it matches the given regex
+   * 
+   * @param {InputEvent} param0 
+   */
+
+  name({ target: nameInput }) {
+    const name = nameInput.value
+
+    nameInput.setCustomValidity("")
+
+    if (name.length < 2) {
+      nameInput.setCustomValidity(`Le ${nameEnum[nameInput.id]} doit faire au minimum 2 caractères`);
+    } else if (/^[a-z-À-ö\s]{2,}$/i.test(name) === false) {
+      nameInput.setCustomValidity(`Le ${nameEnum[nameInput.id]} contient des caractères invalide`);
+    }
+
+    nameInput.reportValidity()
+  },
+
+  email({ target: emailInput }) {
+    const email = emailInput.value
+    const emailRegex = /^[a-zA-Z]+[a-z-A-Z.-_\d]+?@[a-zA-Z]+\.[a-z]{2,4}$/;
+
+    emailInput.setCustomValidity("")
+
+    if (emailRegex.test(email) === false) {
+      emailInput.setCustomValidity("Format de l'email incorrect")
+    }
+
+    emailInput.reportValidity()
+  },
+
+  message({ target: messageInput }) {
+    const message = messageInput.value
+
+    messageInput.setCustomValidity("")
+
+    if (message.length < 20) {
+      messageInput.setCustomValidity("Votre message doit être supérieur à 20 caractères")
+    } else if (message.length >= 150) {
+      messageInput.setCustomValidity("Votre message ne peut pas dépasser 150 caractères")
+      messageInput.value = messageInput.value.slice(0, -1)
+    }
+
+    messageInput.reportValidity()
+  }
+}
+
