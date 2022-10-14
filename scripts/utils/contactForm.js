f625d5baea81aa8e205d9e54c9b3b2b98ac04fe7
David Michel <david.michel2809@gmail.com>
2022-10-14 13:49:03 +0200

    feat: adding a form reset method on closeModal, adding comments to contactForm.js #13

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,21 @@
+import { form } from "../pages/photographer/constants.js"
+
+/**
+ * Changing display mode of contact modal to block
+ */
+
 export function displayModal() {
     const modal = document.getElementById("contact_modal")
     modal.style.display = "block"
 }
 
+/**
+ * Changing display mode of contact modal to none
+ * reseting form inputs
+ */
+
 export function closeModal() {
     const modal = document.getElementById("contact_modal")
     modal.style.display = "none"
+    form.reset()
 }
