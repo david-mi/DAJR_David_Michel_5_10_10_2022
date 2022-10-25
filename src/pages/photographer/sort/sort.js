3f4c59d2937de6b659979a642cfb85d6852f6053
David Michel <david.michel2809@gmail.com>
2022-10-25 12:58:22 +0200

    refacto: adding more details to sort.js #17

--- src/pages/photographer/sort/sort.js
+++ src/pages/photographer/sort/sort.js
@@ -1,11 +1,37 @@
+import "../../../data/types.js";
+
+/**
+ * Contains all callbacks that will be applied to Array.sort method
+ */
+
 export const sortCallbacks = {
-  date(a, b) {
-    return Date.parse(a.date) - Date.parse(b.date);
+  /**
+   * @param {mediaType} nextMedia  next media in Array.sort loop
+   * @param {mediaType} currentMedia current media in Array.sort loop
+   * @returns {number} return value of Array.sort callback call for each iteration 
+   */
+
+  date(nextMedia, currentMedia) {
+    return Date.parse(nextMedia.date) - Date.parse(currentMedia.date);
   },
-  popularity(a, b) {
-    return b.likes - a.likes;
+
+  /**
+   * @param {mediaType} nextMedia  next media in Array.sort loop
+   * @param {mediaType} currentMedia current media in Array.sort loop
+   * @returns {number} return value of Array.sort callback call for each iteration 
+   */
+
+  popularity(nextMedia, currentMedia) {
+    return currentMedia.likes - nextMedia.likes;
   },
-  title(a, b) {
-    return a.title.localeCompare(b.title);
+
+  /**
+   * @param {mediaType} nextMedia  next media in Array.sort loop
+   * @param {mediaType} currentMedia current media in Array.sort loop
+   * @returns {number} return value of Array.sort callback call for each iteration 
+   */
+
+  title(nextMedia, currentMedia) {
+    return nextMedia.title.localeCompare(currentMedia.title);
   }
 };
