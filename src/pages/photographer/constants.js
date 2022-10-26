50326f9e262ca2e63f99976b64a9500be11fe52b
David Michel <david.michel2809@gmail.com>
2022-10-26 14:16:32 +0200

    feat: adding header and main to constants #33

--- src/pages/photographer/constants.js
+++ src/pages/photographer/constants.js
@@ -2,3 +2,5 @@ export const form = document.querySelector(".modal form");
 export const lightboxContainer = document.querySelector(".lightbox-container");
 export const lightboxMediaContainer = document.querySelector(".lightbox-media-container");
 export const photographerMediasElement = document.getElementById("medias");
+export const header = document.getElementById("header");
+export const main = document.getElementById("main");
