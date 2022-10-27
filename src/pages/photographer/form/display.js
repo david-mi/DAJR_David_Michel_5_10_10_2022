37e3192d9b29b41b58bdf7f4fd02618fb0fda958
David Michel <david.michel2809@gmail.com>
2022-10-27 10:58:07 +0200

    feat: prevent tabbing outside form modal with tabindexes #36

--- src/pages/photographer/form/display.js
+++ src/pages/photographer/form/display.js
@@ -1,23 +1,38 @@
 import { form } from "../constants.js";
 
+const modal = document.getElementById("contact_modal");
+
 /**
  * Changing display mode of contact modal to block
+ * Adding tabindex attribute at -1 on every focusable 
+ * elements who aren't inside form modal {@link focusableElementsOutsideForm}
  */
 
 export const displayModal = () => {
-  const modal = document.getElementById("contact_modal");
+  const focusableElementsOutsideForm = document.querySelectorAll("button:not(.modal button), a, select");
   modal.classList.add("display-modal");
   document.body.classList.add("overflow");
+
+  focusableElementsOutsideForm.forEach(element => {
+    element.tabIndex = -1;
+  });
 };
 
 /**
  * Changing display mode of contact modal to none
+ * Removing tabindex attribute on every focusable 
+ * elements who aren't inside form modal {@link focusableElementsOutsideForm}
  * reseting form inputs
  */
 
 export const closeModal = () => {
-  const modal = document.getElementById("contact_modal");
+  const focusableElementsOutsideForm = document.querySelectorAll("button:not(.modal button), a, select");
   modal.classList.remove("display-modal");
   document.body.classList.remove("overflow");
+
+  focusableElementsOutsideForm.forEach(element => {
+    element.removeAttribute("tabindex");
+  });
+
   form.reset();
 };
